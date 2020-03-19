29ff476343ba57a194b1d182b100fb9356f8822e
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-19 20:46:24 -0300

    Atualizando DAO aula 67 - Delete

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -80,9 +80,6 @@ class Usuario{
         $this->setDtcadastro(new DateTime($data['dtcadastro']));
     }
 
-
-
-
     public function insert(){
         $sql = new Sql();
         $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
@@ -97,13 +94,25 @@ class Usuario{
     public function update($login, $password){
         $this->setDeslogin($login);
         $this->setDessenha($password);
-        
+
         $sql = new Sql();
         $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, desenha = :PASSWORD WHERE idusuario = :ID",array(
             ':LOGIN'=>$this->getDeslogin(),
             ':PASSWORD'=>$this->getDesenha(),
             ':ID'=>$this->getIdusuario()
         ));
+    }
+
+    public function delete(){
+        $sql = new Sql();
+        $sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin(0);
+        $this->setDessenha(0);
+        $this->setDtcadastro(new DateTime());
     }
 
     public function __construct($login = "", $password = ""){
--- DAO/index.php
+++ DAO/index.php
@@ -1,34 +1,45 @@
 <?php
 
 require_once("config.php");
-
+/*
 //Carrega um usuário
 //$jose = new Usuario();
 //$jose->loadByID(3);
 //echo $jose;
-
+*/
+/*
 //Carrega uma lista de usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
-
+*/
+/*
 //Carrega uma lista de usuários buscando pelo login
 //$search = Usuario::search("jo");
 //echo json_encode($search);
-
+*/
+/*
 //Carrega um usuário usando o login e a senha
 //$usuario = new Usuario();
 //$usuario->login("joao", "qwerty");
 //echo $usuario;
-
+*/
+/*
 //Criando um novo usuário
 //$aluno = new Usuario("aluno", "@lun0");
 //$aluno->insert();
 //echo $aluno;
-
-//Update no usuário já existente
+*/
+/*
+//Alterar um usuário já existente
 $usuario = new Usuario();
 $usuario->loadByID(6);
 $usuario->update("Carlos", "12345teste");
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+$usuario->loadByID(6);
+$usuario->delete();
 echo $usuario;
 
 ?>
