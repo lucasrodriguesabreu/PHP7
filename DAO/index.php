24a7f34c5e163ebdd133d127d61f35f87248463d
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-19 18:49:46 -0300

    Atualizando DAO aula 64 - métodos de lista

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+//Carrega um usuário
+//$jose = new Usuario();
+//$jose->loadByID(3);
+//echo $jose;
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("joao", "qwerty");
+
+echo $usuario;
 
 ?>
