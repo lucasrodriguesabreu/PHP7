d8e63fad2a8b89ceefd0e54f1bca2e4f7bfb52be
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-22 07:04:50 -0300

    Estudo de FGETS concluído

--- FGETS/exemplo01.php
+++ FGETS/exemplo01.php
@@ -6,9 +6,23 @@ if(file_exists(filename)){
 
     $file = fopen($filename, "r");
     
-    $headers = fgets($file);
+    $headers = explode(",", fgets($file));
 
-    var_dump($headers);
+    $data = array();
+    
+    while($row = fgets($file)){
+        $rowData = explode(",", $row);
+        $linha = array();
+
+        for ($i = 0; $i < count($headers); $i++){
+            $linha[$headers[$i]] = $rowData[$i];
+        }
+        array_push($data, $linha);
+    }
+    
+    fclose($file);
+
+    echo json_encode($data);
 }
 
 ?>
