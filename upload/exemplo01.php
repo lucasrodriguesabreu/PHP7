ace3fee8664019e6f6f51be16b8c98d99d417173
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-22 08:43:43 -0300

    Estudando Upload - Aula 72 - Ainda não concluído

---
+++ upload/exemplo01.php
@@ -0,0 +1,19 @@
+<form method="POST" enctype="multipart/form-data">
+
+    <input type="file" name="fileUpload">
+
+    <button type="submit">Send</button>
+
+</form>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+}
+
+
+?>
+
+
+//PAREI COM 4:20 AULA 72 - UPLOAD DE ARQUIVOS COM PHP7
