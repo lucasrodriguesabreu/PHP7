918c9e8b531196f3ee1529315dbf772b3b9b64d7
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-22 21:07:33 -0300

    Aula de Upload concluída

--- upload/exemplo01.php
+++ upload/exemplo01.php
@@ -10,10 +10,28 @@
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 
+    $file = $_FILES["fileUpload"];
+
+    if($file["error"]){
+        throw new Exception("Error: ".$file["error"]);
+    }
+
+    $dirUploads = "uploads";
+
+    if(!is_dir($dirUploads)){
+
+        mkdir($dirUploads);
+
+    }
+
+    if (move_uploaded_file($file["tmp_name"], $dirUploads . DIRECTORY_SEPARATOR . $file["name"])){
+
+        echo "Upload realizado com sucesso!";
+
+    }else{
+
+        throw new Exception("Não foi possível realizar o upload.");
+
+    }
 }
-
-
 ?>
-
-
-//PAREI COM 4:20 AULA 72 - UPLOAD DE ARQUIVOS COM PHP7
