31717d6e30f92c9646c3f76cfa9c22a60e3df591
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-25 04:00:02 -0300

    Estudo de css concluído

---
+++ xss/exemplo01.php
@@ -0,0 +1,22 @@
+<form method="post">
+	
+	<input type="text" name="busca">
+	<button type="submit">Enviar</button>
+
+
+</form>
+
+
+<?php
+
+$_POST['busca'] = '<a href="#"><strong>Oi</strong></a><script>alert("Ok")</script>';
+
+if(isset($_POST['busca'])){
+
+	//Removendo tags e adicionando excessões, após a vírgula do busca.
+	//echo strip_tags($_POST['busca'], "<strong><a>");
+	//Escrevendo todas as tags
+	echo htmlentities($_POST['busca']);
+}
+
+?>
